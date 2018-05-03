1556b8abfc21879d130979b0ecbc36fa038b89ae
Lou Smith <louann111@comcast.net>
2018-05-03 13:36:51 -0700

    my quicksort program

---
+++ my_quicksort.py
@@ -0,0 +1,37 @@
+def quicksorting(l):
+
+	if len(l) < 2:
+		return(l)
+		
+	left = []
+	right = []
+	equal = []
+	pivot = l[0]
+	equal.append(pivot)
+
+	for i in l[1:]:
+		if pivot > i:
+			left.append(i)
+		elif pivot < i:
+			right.append(i)
+		else:
+			equal.append(i)
+		
+	newlist = quicksorting(left) + equal + quicksorting(right)
+	
+	return(newlist)
+
+
+
+if __name__ == "__main__":
+	f = open("C:\Users\louan\Git\my_project\my_quicksort_input.txt", 'r')
+	
+	int_list = map(int, (f.read().split()))
+	
+	result = quicksorting(int_list)
+	
+	print int_list
+	
+	print result
+	
+	f.close()
